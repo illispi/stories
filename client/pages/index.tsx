f5b1b3747b8050430465b8cc6b33f5eab4c143ae
illispi <44943503+illispi@users.noreply.github.com>
2022-06-18 10:36:04 +0300

    added todo.md

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started commit by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
---
+++ todo.md
@@ -0,0 +1,3 @@
+unified way to build and run dev for server.
+
+check the best have node dev for typescript projects.
