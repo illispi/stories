c1cc4945f263c15120db2c49ab1b65d6b4e5109e
illispi <44943503+illispi@users.noreply.github.com>
2022-06-27 12:49:26 +0300

    context added to trpc and createCookie route

--- apps/server/src/context.ts
+++ apps/server/src/context.ts
@@ -2,9 +2,15 @@ import { inferAsyncReturnType } from "@trpc/server";
 import { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
 
 export function createContext({ req, res }: CreateFastifyContextOptions) {
-  const user = { name: req.headers.username ?? "anonymous" };
+  const user = { id: req.session.id };
 
   return { req, res, user };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
+
+declare module "fastify" {
+  interface Session {
+    id: string;
+  }
+}
--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -1,11 +1,12 @@
 import * as trpc from "@trpc/server";
+import { Context } from "./context";
 // import { z } from "zod";
 import { db } from "./index";
 // import { createUser } from "zod-types";
 // import { sql } from "kysely";
 
 export const appRouter = trpc
-  .router()
+  .router<Context>()
   .mutation("createUser", {
     resolve: async () => {
       const { user_id } = await db
@@ -26,7 +27,39 @@ export const appRouter = trpc
 
       return allUsersIds;
     },
+  })
+  .mutation("createCookie", {
+    resolve: async ({ ctx }) => {
+      if (!ctx.user.id) {
+        const { user_id } = await db
+          .insertInto("user")
+          .returning("user_id")
+          .executeTakeFirstOrThrow();
+
+        ctx.req.session.id = user_id;
+
+        return "There should be cookies";
+      }
+      return "There already should be cookie";
+    },
   });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
+// if (!request.session.myId) {
+//   const user = await prisma.user.create({ data: {} });
+
+//   request.session.myId = user.id;
+
+//   reply.send({ status: "profile created!" });
+// } else {
+//   const user = await prisma.user.findUnique({
+//     where: { id: request.session.myId },
+//   });
+//   if (!user?.userName) {
+//     reply.status(200).send({ status: "Not registered" });
+//   } else {
+//     reply.status(200).send({ status: "Authenticated!" });
+//   }
+// }
