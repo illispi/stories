ee0812dbf6b717e411e71e1c8bbee53589d1a717
illispi <44943503+illispi@users.noreply.github.com>
2022-06-30 16:57:20 +0300

    wasted time trying get types to js

---
+++ apps/client/src/components/UnitQuestion.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { personalQuestionsSchema, PersonalQuestions } from "zod-types";
+
+//NOTE might need yes or no selection
+
+type QuestionType = "selection" | "integer" | "text";
+
+export const UnitQuestion: React.FC<{
+  question: string;
+  question_db: keyof PersonalQuestions;
+  questionType: QuestionType;
+  selections?: string[];
+}> = (props) => {
+  //NOTE Do i need to validate?
+
+  type QuestionPick = Pick<
+    PersonalQuestions,
+    typeof props.question_db
+  >[typeof props.question_db];
+
+  const handleSubmit = (value: QuestionPick) => {
+    try {
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(props.question_db, serializedValue);
+    } catch (err) {
+      console.log(err);
+      return undefined;
+    }
+  };
+
+  if (props.questionType === "selection") {
+    return (
+      <div>
+        <h3>{props.question}</h3>
+        {props.selections?.map((v) => (
+          <button
+            key={`key${props.question_db}${v}`}
+            onClick={() => handleSubmit(v)}
+          >
+            Male
+          </button>
+        ))}
+      </div>
+    );
+  }
+  if (props.questionType === "integer") {
+    return <h2>wip</h2>;
+  }
+  if (props.questionType === "text") {
+    return <h2>wip</h2>;
+  }
+
+  return null;
+};
--- apps/client/src/pages/personalQuestions.tsx
+++ apps/client/src/pages/personalQuestions.tsx
@@ -1,40 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { personalQuestionsSchema, PersonalQuestions } from "zod-types";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+
 
 //TODO indiviual things are saved to local storage and then you combine them and send to server
 
 const PersonalQuestions = () => {
-  //NOTE How to do this inside useForm<>?
-
-  type returnType = Pick<PersonalQuestions, "gender">;
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<returnType>({
-    resolver: zodResolver(
-      personalQuestionsSchema.pick({ gender: true }).required()
-    ),
-  });
-  const onSubmit = (data: returnType) => console.log(data.gender);
-
-  return (
-    <div>
-      <h3>What is your gender?</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <button {...register("gender")}>Male</button>
-        <button {...register("gender")} value="female">
-          Female
-        </button>
-
-        <button {...register("gender")}>Other</button>
-      </form>
-    </div>
-  );
+ return <p>work in progress</p>
 };
 
 export default PersonalQuestions;
