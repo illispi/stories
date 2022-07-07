70ccdaecfdf090e8452f824f424e98cfb6185bcc
illispi <44943503+illispi@users.noreply.github.com>
2022-07-07 16:27:51 +0300

    allowed zoom, improving accessibility

--- apps/client/src/pages/personalQuestions.tsx
+++ apps/client/src/pages/personalQuestions.tsx
@@ -66,7 +66,7 @@ const PersonalQuestions = () => {
         <title>Questionare</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=4.0"
         />
       </Head>
       <paginationContext.Provider value={{ page, direction, paginate }}>
