18bb8dd8dc12d217b2bed48cfdc9b2c92ebf90b9
illispi <44943503+illispi@users.noreply.github.com>
2022-07-12 10:57:00 +0300

    navbar wip progress

--- apps/client/src/components/NavBar.tsx
+++ apps/client/src/components/NavBar.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="sticky top-0 h-12 w-full bg-gradient-to-b  from-green-500 to-green-600 "></div>
+    <div className="sticky top-0 flex h-12 w-full items-center bg-gradient-to-b from-blue-200 to-blue-300">
+      <Link href={"/"}>
+        <a className="mx-2 text-2xl font-semibold sm:hidden">
+          Schizo... stories
+        </a>
+      </Link>
+      <Link href={"/"}>
+        <a className="mx-2 hidden text-2xl font-semibold sm:flex">
+          Schizophrenia stories
+        </a>
+      </Link>
+    </div>
   );
 };
 
