74547304f580510399fe98cbba8020048edc80bf
illispi <44943503+illispi@users.noreply.github.com>
2022-08-16 11:21:55 +0300

    wip for stats

--- apps/client/src/pages/stats.tsx
+++ apps/client/src/pages/stats.tsx
@@ -5,9 +5,11 @@ import { PersonalQuestions } from "zod-types";
 
 const Item = ({ name, item }: { name: string; item: string | number }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-8">
-      <p className="text-lg">{name} </p>
-      <p>{item}</p>
+    <div className="my-3 flex flex-col items-center justify-center">
+      <p className="mb-2 text-center text-lg">{name} </p>
+      <p className="rounded-full  border-2 border-slate-400 p-4 text-center font-semibold">
+        {item}
+      </p>
     </div>
   );
 };
@@ -28,11 +30,19 @@ const Stats: NextPage = () => {
 
   return (
     <div className="mt-8 flex w-screen flex-col items-center justify-center">
-      <div className="flex flex-col  overflow-hidden rounded-3xl bg-white shadow-xl shadow-slate-500 max-w-xs w-11/12">
+      <div className="flex w-11/12  max-w-xs flex-col overflow-hidden rounded-3xl bg-white shadow-xl shadow-slate-500">
         <div className="flex h-16 items-center justify-center bg-blue-300 p-4">
           <h1 className="text-center font-semibold">Personal Stats</h1>
         </div>
         <div>
+          <Item
+            name={"Total responses:"}
+            item={`${personalStats.data.length}`}
+          ></Item>
+          <Item
+            name={"Gender proportions:"}
+            item={`${calcAverage(personalStats, "current_age")} years old`}
+          ></Item>
           <Item
             name={"Average age of responses:"}
             item={`${calcAverage(personalStats, "current_age")} years old`}
