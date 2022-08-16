7e29a6857edaa644832f67c1d4ccc559f19e8b94
illispi <44943503+illispi@users.noreply.github.com>
2022-08-16 11:08:01 +0300

    stats page wip

--- apps/client/src/pages/stats.tsx
+++ apps/client/src/pages/stats.tsx
@@ -1,18 +1,44 @@
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import React from "react";
+import { PersonalQuestions } from "zod-types";
+
+const Item = ({ name, item }: { name: string; item: string | number }) => {
+  return (
+    <div className="flex flex-col items-center justify-center p-8">
+      <p className="text-lg">{name} </p>
+      <p>{item}</p>
+    </div>
+  );
+};
 
 const Stats: NextPage = () => {
   const personalStats = trpc.useQuery(["personalStats"]);
-  console.log(personalStats.data);
+
+  const calcAverage = (
+    arr: typeof personalStats,
+    key: keyof PersonalQuestions
+  ) => {
+    return arr.data.reduce((a, b) => a + b[key], 0) / arr.data.length;
+  };
+
+  if (!personalStats.data) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
-    <div>
-      <p>Average age of responses:</p>
-      <span>
-        {personalStats.data?.reduce((a, b) => a + b.current_age, 0) /
-          personalStats.data?.length}
-      </span>
+    <div className="mt-8 flex w-screen flex-col items-center justify-center">
+      <div className="flex flex-col  overflow-hidden rounded-3xl bg-white shadow-xl shadow-slate-500 max-w-xs w-11/12">
+        <div className="flex h-16 items-center justify-center bg-blue-300 p-4">
+          <h1 className="text-center font-semibold">Personal Stats</h1>
+        </div>
+        <div>
+          <Item
+            name={"Average age of responses:"}
+            item={`${calcAverage(personalStats, "current_age")} years old`}
+          />
+        </div>
+      </div>
     </div>
   );
 };
