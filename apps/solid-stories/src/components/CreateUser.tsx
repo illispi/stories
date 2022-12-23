0571d54659eab98c5757ab70254620eb2a5027e7
illispi <44943503+illispi@users.noreply.github.com>
2022-12-23 16:44:57 +0200

    clean up

--- apps/solid-stories/src/components/CreateUser.tsx
+++ apps/solid-stories/src/components/CreateUser.tsx
@@ -1,11 +1,9 @@
+import type { Component } from "solid-js";
 import { createServerData$ } from "solid-start/server";
 import { createOrGetUser } from "~/server/server";
 
 const CreateUser: Component<{}> = (props) => {
-  const data = createServerData$(async (_, { request }) =>
-    createOrGetUser(request)
-  );
-  console.log(data());
+  createServerData$(async (_, { request }) => createOrGetUser(request));
 
   return null;
 };
