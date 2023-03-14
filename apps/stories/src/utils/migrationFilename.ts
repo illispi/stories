8277d4baa38cb54fd40b6ba0e8e9cb654bd3f4b7
illispi <a@a.com>
2023-03-14 06:14:54 +0200

    fixed iso8601

--- apps/stories/src/utils/migrationFilename.ts
+++ apps/stories/src/utils/migrationFilename.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath } from 'node:url'
-import { writeFileSync } from 'node:fs'
-import { resolve } from 'node:path'
+import { fileURLToPath } from "node:url";
+import { writeFileSync } from "node:fs";
+import { resolve } from "node:path";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-const date = new Date()
-const year = date.getFullYear()
-const month = date.getMonth() + 1
-const day = date.getDate()
-const hours = date.getHours()
-const minutes = date.getMinutes()
-const seconds = date.getSeconds()
+const convertToIso = (n: number) => {
+  return n.toString().length > 1 ? n : `0${n.toString()}`;
+};
+
+const date = new Date();
+const year = date.getFullYear();
+const month = convertToIso(date.getMonth() + 1);
+const day = convertToIso(date.getDate());
+const hours = convertToIso(date.getHours());
+const minutes = convertToIso(date.getMinutes());
+const seconds = convertToIso(date.getSeconds());
 
 const code = `import type { Kysely } from 'kysely'
 import { sql } from 'kysely'
@@ -21,9 +25,9 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 export async function down(db: Kysely<any>): Promise<void> {
 
-}`
+}`;
 
-const name = process.argv.slice(2).join('_').toLowerCase()
+const name = process.argv.slice(2).join("_").toLowerCase();
 
 writeFileSync(
   resolve(
@@ -31,4 +35,4 @@ writeFileSync(
     `../migrations/${year}_${month}_${day}_${hours}_${minutes}_${seconds}_${name}.ts`
   ),
   code
-)
+);
