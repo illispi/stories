e1191d9ceb777d84f8dae526ef2ec66b2f776045
illispi <44943503+illispi@users.noreply.github.com>
2023-03-14 16:58:13 +0200

    fixed text component being empty

--- apps/stories/src/routes/stats/(stats).tsx
+++ apps/stories/src/routes/stats/(stats).tsx
@@ -203,26 +203,31 @@ const TextComponent: Component<{
   const personalStats = useData();
 
   return (
-    <div class="flex w-11/12 max-w-xs flex-col items-center justify-center">
-      <h4 class="m-2 text-center text-xl underline underline-offset-8">{`${props.header}:`}</h4>
-      <Index each={personalStats?.data[props.stat]}>
-        {(stat, i) => (
-          <div class="flex w-full max-w-xs flex-col items-center justify-center">
-            <h5 class="m-2 font-bold">{i + 1}.</h5>
-            <p class="w-full">{stat()}</p>
-          </div>
-        )}
-      </Index>
+    <Show
+      when={personalStats?.data[props.stat].length > 0}
+      fallback={<div>failure</div>}
+    >
+      <div class="flex w-11/12 max-w-xs flex-col items-center justify-center">
+        <h4 class="m-2 text-center text-xl underline underline-offset-8">{`${props.header}:`}</h4>
+        <Index each={personalStats?.data[props.stat]}>
+          {(stat, i) => (
+            <div class="flex w-full max-w-xs flex-col items-center justify-center">
+              <h5 class="m-2 font-bold">{i + 1}.</h5>
+              <p class="w-full">{stat()}</p>
+            </div>
+          )}
+        </Index>
 
-      <A href={`${props.stat}`}>
-        <div
-          class="m-2 my-8 rounded-full bg-blue-500 p-3 font-semibold
+        <A href={`${props.stat}`}>
+          <div
+            class="m-2 my-8 rounded-full bg-blue-500 p-3 font-semibold
       text-white transition-all hover:scale-110 hover:bg-blue-600 active:scale-110 active:bg-blue-600"
-        >
-          Show more
-        </div>
-      </A>
-    </div>
+          >
+            Show more
+          </div>
+        </A>
+      </div>
+    </Show>
   );
 };
 
