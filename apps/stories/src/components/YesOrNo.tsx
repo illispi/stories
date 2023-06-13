822305eca33a547ddef3a4da68831c4f0e061820
illispi <44943503+illispi@users.noreply.github.com>
2023-06-13 11:55:45 +0300

    fixed previous commit problem

--- apps/stories/src/components/YesOrNo.tsx
+++ apps/stories/src/components/YesOrNo.tsx
@@ -1,4 +1,4 @@
-import { Component, Show, createSignal } from "solid-js";
+import { Component, Show, createEffect, createSignal } from "solid-js";
 import { MainReturn } from "~/types/types";
 import { PersonalQuestions } from "~/types/zodFromTypes";
 import PieChartCustom from "./PieChartCustom";
@@ -8,32 +8,37 @@ export const YesOrNoComponent: Component<{
   header: string;
   data: MainReturn | undefined;
 }> = (props) => {
+  const [questionData, setQuestionData] = createSignal(null);
+
+  createEffect(() => {
+    if (props.data) {
+      setQuestionData(props.data[props.stat]);
+    } else {
+      setQuestionData(null);
+    }
+  });
+
   return (
     <div class="flex flex-col items-center justify-center">
       <h4 class="m-2 text-center text-xl underline underline-offset-8">{`${props.header}:`}</h4>
       <div class="mb-4 flex w-11/12 items-center justify-center lg:max-w-xs">
         <PieChartCustom
           data={
-            props.data
+            questionData()
               ? {
                   labels: [
                     `Yes ${Math.floor(
-                      (props.data[props.stat].yes /
-                        (props.data[props.stat].yes +
-                          props.data[props.stat].no)) *
+                      (questionData().yes /
+                        (questionData().yes + questionData().no)) *
                         100
                     )}%`,
                     `No ${Math.floor(
-                      (props.data[props.stat].no /
-                        (props.data[props.stat].yes +
-                          props.data[props.stat].no)) *
+                      (questionData().no /
+                        (questionData().yes + questionData().no)) *
                         100
                     )}%`,
                   ],
-                  series: [
-                    props.data[props.stat].yes,
-                    props.data[props.stat].no,
-                  ],
+                  series: [questionData().yes, questionData().no],
                 }
               : null
           }
