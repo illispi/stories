ce3f1aaf9412cfef75d077e0d51f360bc72956b8
illispi <44943503+illispi@users.noreply.github.com>
2023-06-17 07:45:39 +0300

    reactivity problem yes or no again fixed

--- apps/stories/src/components/YesOrNo.tsx
+++ apps/stories/src/components/YesOrNo.tsx
@@ -2,43 +2,37 @@ import { Component, Show, createEffect, createSignal } from "solid-js";
 import { MainReturn } from "~/types/types";
 import { PersonalQuestions } from "~/types/zodFromTypes";
 import PieChartCustom from "./PieChartCustom";
-import { useData } from "./globalSignals";
 
 export const YesOrNoComponent: Component<{
   stat: keyof MainReturn;
   header: string;
   data: MainReturn | undefined;
 }> = (props) => {
+  const [qDAata, setQDAata] = createSignal(null);
+
+  createEffect(() => {
+    setQDAata({
+      labels: [
+        `Yes ${Math.floor(
+          (props.data?.[props.stat].yes /
+            (props.data?.[props.stat].yes + props.data?.[props.stat].no)) *
+            100
+        )}%`,
+        `No ${Math.floor(
+          (props.data?.[props.stat].no /
+            (props.data?.[props.stat].yes + props.data?.[props.stat].no)) *
+            100
+        )}%`,
+      ],
+      series: [props.data?.[props.stat].yes, props.data?.[props.stat].no],
+    });
+  });
+
   return (
     <div class="flex flex-col items-center justify-center">
       <h4 class="m-2 text-center text-xl underline underline-offset-8">{`${props.header}:`}</h4>
       <div class="mb-4 flex w-11/12 items-center justify-center lg:max-w-xs">
-        <PieChartCustom
-          data={
-            props.data?.[props.stat]
-              ? {
-                  labels: [
-                    `Yes ${Math.floor(
-                      (props.data?.[props.stat].yes /
-                        (props.data?.[props.stat].yes +
-                          props.data?.[props.stat].no)) *
-                        100
-                    )}%`,
-                    `No ${Math.floor(
-                      (props.data?.[props.stat].no /
-                        (props.data?.[props.stat].yes +
-                          props.data?.[props.stat].no)) *
-                        100
-                    )}%`,
-                  ],
-                  series: [
-                    props.data?.[props.stat].yes,
-                    props.data?.[props.stat].no,
-                  ],
-                }
-              : null
-          }
-        />
+        <PieChartCustom data={qDAata()} />
       </div>
     </div>
   );
