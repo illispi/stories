a1e1f2ce66a9d3ee76e0ab78fbbb869ed9bd9d7d
illispi <44943503+illispi@users.noreply.github.com>
2023-07-03 10:29:23 +0300

    solved with custom Trans, could prob done wit MOne

--- apps/stories/src/components/ModalPopUp.tsx
+++ apps/stories/src/components/ModalPopUp.tsx
@@ -1,8 +1,59 @@
 import { Presence, Motion } from "@motionone/solid";
-import type { ParentComponent, Setter } from "solid-js";
+import type { Component, ParentComponent, ParentProps, Setter } from "solid-js";
 import { Show, createEffect, createSignal } from "solid-js";
 import { Portal } from "solid-js/web";
 import { twMerge } from "tailwind-merge";
+import { createSwitchTransition } from "@solid-primitives/transition-group";
+import { resolveFirst } from "@solid-primitives/refs";
+
+const Transition: Component<ParentProps> = (props) => {
+  const el = resolveFirst(() => props.children);
+
+  const rendered = createSwitchTransition(el, {
+    onEnter(el, done) {
+      // the enter callback is called before the element is inserted into the DOM
+      // so run the animation in the next animation frame / microtask
+      queueMicrotask(() => {
+        if (!el.isConnected) return done();
+
+        el.animate(
+          [
+            {
+              opacity: 0,
+              transform: "translate(-50%, -100px)",
+              easing: "ease-in",
+            },
+            { opacity: 1, transform: `translate(-50%, 0)`, easing: "ease-out" },
+          ],
+          { duration: 300 }
+        )
+          .finished.then(done)
+          .catch(done);
+        /*...*/
+      });
+    },
+    onExit(el, done) {
+      if (!el.isConnected) return done();
+
+      el.animate(
+        [
+          { opacity: 1, transform: `translate(-50%, 0)`, easing: "ease-out" },
+          {
+            opacity: 0,
+            transform: "translate(-50%, -100px)",
+            easing: "ease-in",
+          },
+        ],
+        { duration: 300 }
+      )
+        .finished.then(done)
+        .catch(done);
+      /*...*/
+    },
+  });
+
+  return <>{rendered()}</>;
+};
 
 const ModalPopUp: ParentComponent<{
   message: string | null;
@@ -10,7 +61,7 @@ const ModalPopUp: ParentComponent<{
   setMessage: Setter<string | null>;
 }> = (props) => {
   const classes = twMerge(
-    "absolute top-24 left-1/2 -translate-x-1/2 rounded-3xl border-2 border-red-400 bg-red-100 p-8 text-center shadow-xl",
+    "absolute top-48 left-1/2 -translate-x-1/2 rounded-3xl border-2 border-red-400 bg-red-100 p-8 text-center shadow-xl",
     props.class
   );
 
@@ -24,19 +75,13 @@ const ModalPopUp: ParentComponent<{
 
   return (
     <Portal>
-      <Presence>
+      <Transition>
         <Show when={props.message}>
-          <Motion.div
-            class={classes}
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -100, opacity: 0 }}
-            transition={{ duration: 0.15 }}
-          >
+          <div class={classes}>
             <p>{props.message}</p>
-          </Motion.div>
+          </div>
         </Show>
-      </Presence>
+      </Transition>
     </Portal>
   );
 };
