6659c2784dfb00660b53414cb03fa8a5d81bd816
illispi <44943503+illispi@users.noreply.github.com>
2023-07-03 10:04:30 +0300

    fixed few issues but translate conflicts with MOne

--- apps/stories/src/components/ModalPopUp.tsx
+++ apps/stories/src/components/ModalPopUp.tsx
@@ -1,17 +1,17 @@
 import { Presence, Motion } from "@motionone/solid";
 import type { ParentComponent, Setter } from "solid-js";
-import { createEffect, createSignal } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
 import { Portal } from "solid-js/web";
 import { twMerge } from "tailwind-merge";
 
 const ModalPopUp: ParentComponent<{
   message: string | null;
-  customClasses?: string;
+  class?: string;
   setMessage: Setter<string | null>;
 }> = (props) => {
   const classes = twMerge(
-    "fixed top-2 rounded-3xl border-4 border-red-600 bg-red-200 p-8 text-center shadow-xl",
-    props.customClasses
+    "absolute top-24 left-1/2 -translate-x-1/2 rounded-3xl border-2 border-red-400 bg-red-100 p-8 text-center shadow-xl",
+    props.class
   );
 
   createEffect(() => {
@@ -23,19 +23,21 @@ const ModalPopUp: ParentComponent<{
   });
 
   return (
-    <Presence>
-      {props.message && (
-        <Motion.div
-          class={classes}
-          initial={{ y: -100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: -100, opacity: 0 }}
-          transition={{ duration: 0.15 }}
-        >
-          <p>{props.message}</p>
-        </Motion.div>
-      )}
-    </Presence>
+    <Portal>
+      <Presence>
+        <Show when={props.message}>
+          <Motion.div
+            class={classes}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -100, opacity: 0 }}
+            transition={{ duration: 0.15 }}
+          >
+            <p>{props.message}</p>
+          </Motion.div>
+        </Show>
+      </Presence>
+    </Portal>
   );
 };
 
