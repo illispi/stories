1e54e12442d6eff1dde1747edd53ec1ef8aa5920
illispi <44943503+illispi@users.noreply.github.com>
2023-07-24 09:18:19 +0300

    Removed kysely_adapter and zod-types packages

--- apps/stories/src/server/mutations.ts
+++ apps/stories/src/server/mutations.ts
@@ -1,7 +1,10 @@
 import { getSession } from "@auth/solid-start";
 import { mutation$ } from "@prpc/solid";
 import { authOpts } from "~/routes/api/auth/[...solidauth]";
-import { personalQuestionsSchema } from "~/types/zodFromTypes";
+import {
+  personalQuestionsSchema,
+  theirQuestionsSchema,
+} from "~/types/zodFromTypes";
 import { db } from "./server";
 export const postPersonalStats = mutation$({
   mutationFn: async ({ payload, request$ }) => {
@@ -29,6 +32,36 @@ export const postPersonalStats = mutation$({
     }
     return "failed to insert";
   },
-  key: "postPersonalStats", // this will be used the mutation key
+  key: "postPersonalStats",
   schema: personalQuestionsSchema,
-}); // this will be used as the input type and input validation
+});
+export const postTheriStats = mutation$({
+  mutationFn: async ({ payload, request$ }) => {
+    const session = await getSession(request$, authOpts);
+
+    if (!session) {
+      return "no session found";
+    }
+
+    const user = await db
+      .selectFrom("User")
+      .select("id")
+      .where("id", "=", session.user.id)
+      .executeTakeFirstOrThrow();
+
+    if (user?.id) {
+      const insertion = await db
+        .insertInto("Their_questions")
+        .values({ ...payload, user: user.id })
+        .execute();
+
+      if (insertion) {
+        return "Added succesfully";
+      }
+    }
+    return "failed to insert";
+  },
+  key: "postPersonalStats",
+  schema: theirQuestionsSchema,
+});
+
