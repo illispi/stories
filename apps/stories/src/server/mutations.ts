c6622ea320077d6bc727b197746e657593099f29
illispi <44943503+illispi@users.noreply.github.com>
2023-08-09 11:21:56 +0300

    hopeless first try for lucia

--- apps/stories/src/server/mutations.ts
+++ apps/stories/src/server/mutations.ts
@@ -1,44 +1,32 @@
-import { getSession } from "@solid-mediakit/auth";
 import { mutation$ } from "@prpc/solid";
 import {
   personalQuestionsSchema,
   theirQuestionsSchema,
 } from "~/types/zodFromTypes";
 
-import { auth } from "./lucia.js";
-import { LuciaError } from "lucia";
+import { auth } from "~/auth/lucia";
 import { db } from "./server";
 export const postPersonalStats = mutation$({
   mutationFn: async ({ payload, request$ }) => {
-    try {
-      const session = await auth.validateSession(request$);
-      if (session.fresh) {
-        // expiration extended
-        const sessionCookie = auth.createSessionCookie(session);
-        setSessionCookie(session);
-        const user = await db
-          .selectFrom("User")
-          .select("id")
-          .where("id", "=", session.user?.id)
-          .executeTakeFirstOrThrow();
+    const authRequest = auth.handleRequest(request$);
+    const session = await authRequest.validate();
+    if (session) {
+      const user = await db
+        .selectFrom("User")
+        .select("id")
+        .where("id", "=", session.user?.id)
+        .executeTakeFirstOrThrow();
 
-        if (user?.id) {
-          const insertion = await db
-            .insertInto("Personal_questions")
-            .values({ ...payload, user: user.id, accepted: false, fake: false })
-            .execute();
+      if (user?.id) {
+        const insertion = await db
+          .insertInto("Personal_questions")
+          .values({ ...payload, user: user.id, accepted: false })
+          .execute();
 
-          if (insertion) {
-            return "Added succesfully";
-          }
+        if (insertion) {
+          return "Added succesfully";
         }
       }
-    } catch (e) {
-      if (e instanceof LuciaError && e.message === `AUTH_INVALID_SESSION_ID`) {
-        // invalid session
-        deleteSessionCookie();
-      }
-      // unexpected database errors
     }
   },
   key: "postPersonalStats",
@@ -46,29 +34,26 @@ export const postPersonalStats = mutation$({
 });
 export const postTheirStats = mutation$({
   mutationFn: async ({ payload, request$ }) => {
-    const session = await getSession(request$, authOpts);
+    const authRequest = auth.handleRequest(request$);
+    const session = await authRequest.validate();
+    if (session) {
+      const user = await db
+        .selectFrom("User")
+        .select("id")
+        .where("id", "=", session.user?.id)
+        .executeTakeFirstOrThrow();
 
-    if (!session) {
-      return "no session found";
-    }
+      if (user?.id) {
+        const insertion = await db
+          .insertInto("Their_questions")
+          .values({ ...payload, user: user.id, accepted: false })
+          .execute();
 
-    const user = await db
-      .selectFrom("User")
-      .select("id")
-      .where("id", "=", session.user?.id)
-      .executeTakeFirstOrThrow();
-
-    if (user?.id) {
-      const insertion = await db
-        .insertInto("Their_questions")
-        .values({ ...payload, user: user.id, accepted: false, fake: false }) //NOTE this TS error should disappear once you migrate down and up again
-        .execute();
-
-      if (insertion) {
-        return "Added succesfully";
+        if (insertion) {
+          return "Added succesfully";
+        }
       }
     }
-    return "failed to insert";
   },
   key: "postTheirStats",
   schema: theirQuestionsSchema,
