a735d815ba0727cb8c05958886e5be0e75d88fc5
illispi <44943503+illispi@users.noreply.github.com>
2023-08-11 15:27:22 +0300

    more tweaks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,11 +7,11 @@ const Home: Component = () => {
     <div class="grid w-full grid-cols-1 justify-items-center lg:grid-cols-2">
       <div class="col-span-2 row-span-2 min-h-[600px] w-full bg-gradient-angle from-blue-400 to-fuchsia-600 ">
         <div class="flex h-full flex-col items-center justify-evenly lg:flex-row">
-          <h1 class="p-4 text-center text-2xl text-white lg:w-4/12 lg:text-4xl font-mono">
+          <h1 class="p-4 text-center font-mono text-2xl text-white lg:w-4/12 lg:text-4xl">
             User poll & shared articles about schizophrenia
           </h1>
           <img
-            class="w-full max-w-sm lg:max-w-xl object-fill 2xl:max-w-4xl"
+            class="w-full max-w-xl object-fill lg:max-w-[50vw]"
             src="/hero_image.png"
             alt="Picture of poll"
           />
