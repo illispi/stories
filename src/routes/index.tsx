7f17c49d624c31c09da211f85ddb3a9271e58089
illispi <44943503+illispi@users.noreply.github.com>
2023-08-12 16:37:56 +0300

    fine tuning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,13 @@ import { A } from "solid-start";
 const Home: Component = () => {
   return (
     <div class="grid w-full grid-cols-1 justify-items-center lg:grid-cols-2">
-      <div class="col-span-2 row-span-2 w-full bg-gradient-angle from-blue-400 to-fuchsia-600 ">
-        <div class="grid grid-cols-1 items-center md:grid-cols-2">
-          <h1 class="m-12 text-center font-mono text-2xl text-white lg:text-4xl">
+      <div class="col-span-2 row-span-2 flex w-full items-center justify-center bg-gradient-angle from-blue-400 to-fuchsia-600 ">
+        <div class="grid grid-cols-1 items-center justify-items-center lg:grid-cols-2 xl:w-9/12">
+          <h1 class="m-12 max-w-sm text-center font-mono text-2xl text-white md:text-3xl lg:text-4xl 2xl:max-w-2xl 2xl:text-6xl">
             User poll & shared articles about schizophrenia
           </h1>
           <img
-            class=""
+            class="md:w-7/12 lg:w-11/12"
             src="/hero_image.png"
             alt="Picture of poll"
           />
