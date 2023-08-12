a5275b1b7be2f0c883f2f501b2229615f7cf1468
illispi <44943503+illispi@users.noreply.github.com>
2023-08-12 17:19:16 +0300

    buttons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,30 @@ import { A } from "solid-start";
 const Home: Component = () => {
   return (
     <div class="grid w-full grid-cols-1 justify-items-center lg:grid-cols-2">
-      <div class="col-span-2 row-span-2 flex w-full items-center justify-center bg-gradient-angle from-blue-400 to-fuchsia-600 ">
+      <div class="col-span-2 row-span-2 flex w-full items-center justify-center bg-gradient-angle from-blue-400 to-fuchsia-500 ">
         <div class="grid grid-cols-1 items-center justify-items-center lg:grid-cols-2 xl:w-9/12">
-          <h1 class="m-12 max-w-sm text-center font-mono text-2xl text-white md:text-3xl lg:text-4xl 2xl:max-w-2xl 2xl:text-6xl">
+          <h1 class="m-12 max-w-sm text-center font-mono text-2xl text-white md:text-3xl lg:hidden">
             User poll & shared articles about schizophrenia
           </h1>
+          <div class="hidden h-5/6 flex-col justify-around lg:flex">
+            <h1 class="m-12 text-center font-mono text-white lg:text-4xl 2xl:max-w-2xl 2xl:text-5xl">
+              User poll & shared articles about schizophrenia
+            </h1>
+            <div class="mx-12 flex items-center justify-center">
+              <A
+                class="m-8 w-1/2 rounded-full border border-black bg-white p-3 text-center text-xl font-semibold text-black shadow-lg shadow-fuchsia-500 transition-all duration-200 ease-out hover:scale-110 active:scale-125 2xl:text-2xl "
+                href={route("/questionares")}
+              >
+                Take poll
+              </A>
+              <A
+                class="m-8 w-1/2 rounded-full border border-black bg-white p-3 text-center text-xl font-semibold text-black shadow-lg shadow-fuchsia-500 transition-all duration-200 ease-out hover:scale-110 active:scale-125 2xl:text-2xl "
+                href={route("/questionares")}
+              >
+                Share article
+              </A>
+            </div>
+          </div>
           <img
             class="md:w-7/12 lg:w-11/12"
             src="/hero_image.png"
