46ef516129c4de83b3d584b2ea5d88316538ecb9
illispi <44943503+illispi@users.noreply.github.com>
2023-08-16 19:31:53 +0300

    1.7 like show

--- src/routes/articles/(articles).tsx
+++ src/routes/articles/(articles).tsx
@@ -1,12 +1,22 @@
-import { Component, For, Suspense, createSignal } from "solid-js";
+import {
+  Component,
+  ErrorBoundary,
+  For,
+  Show,
+  Suspense,
+  createSignal,
+} from "solid-js";
 import CustomButton from "~/components/CustomButton";
 import { articlesPagination } from "~/server/basic/queries";
 
 const articles: Component = () => {
   const [page, setPage] = createSignal(0);
-  const articlesData = articlesPagination(() => ({ page: page() }));
-
-  console.log(articlesData.data);
+  const articlesData = articlesPagination(
+    () => ({ page: page() }),
+    () => ({
+      placeholderData: (prev) => prev,
+    })
+  );
 
   return (
     <div class="my-16 flex w-full flex-col items-center justify-start gap-4">
@@ -18,34 +28,40 @@ const articles: Component = () => {
           </div>
         )}
       </For>
-      {/* <div class="m-16 flex w-full items-center justify-around">
-          <CustomButton
-            class={page() === 0 ? "invisible" : ""}
-            onClick={() => setPage((prev) => (prev === 0 ? 0 : prev - 1))}
-          >
-            Back
-          </CustomButton>
-          <h5 class="text-lg font-bold">{`Page: ${page() + 1}/${
-            Math.floor(articlesData.data[0].count / 25) + 1
-          }`}</h5>
+      <Suspense>
+        <Show when={articlesData.data}>
+          {(articles) => (
+            <div class="m-16 flex w-full items-center justify-around">
+              <CustomButton
+                class={page() === 0 ? "invisible" : ""}
+                onClick={() => setPage((prev) => (prev === 0 ? 0 : prev - 1))}
+              >
+                Back
+              </CustomButton>
+              <h5 class="text-lg font-bold">{`Page: ${page() + 1}/${
+                Math.floor(articles()[0].count / 25) + 1
+              }`}</h5>
 
-          <CustomButton
-            class={
-              articlesData.data[0].count / ((page() + 1) * 25) <= 1
-                ? "invisible"
-                : ""
-            }
-            onClick={() =>
-              setPage((prev) =>
-                articlesData.data[0].count / ((prev + 1) * 25) <= 1
-                  ? prev
-                  : prev + 1
-              )
-            }
-          >
-            Next
-          </CustomButton>
-        </div> */}
+              <CustomButton
+                class={
+                  articles()[0].count / ((page() + 1) * 25) <= 1
+                    ? "invisible"
+                    : ""
+                }
+                onClick={() =>
+                  setPage((prev) =>
+                    articles()[0].count / ((prev + 1) * 25) <= 1
+                      ? prev
+                      : prev + 1
+                  )
+                }
+              >
+                Next
+              </CustomButton>
+            </div>
+          )}
+        </Show>
+      </Suspense>
     </div>
   );
 };
