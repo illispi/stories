7dbbcc57004c4084883fbbd01edd31c8b7d30bd4
illispi <44943503+illispi@users.noreply.github.com>
2023-09-05 11:04:59 +0300

    articles wip

--- src/routes/articles/(articles).tsx
+++ src/routes/articles/(articles).tsx
@@ -22,38 +22,78 @@ const ArticleSchema = object({
 
 type ArticleForm = Input<typeof ArticleSchema>;
 
-const ArticleSubmit: Component = (props) => {
+const ArticleSubmit: Component<{ setSubmitVis: Setter<boolean> }> = (props) => {
   const articleMut = postArticle();
   const [articleForm, { Form, Field }] = createForm<ArticleForm>({
     validate: valiForm(ArticleSchema),
   });
   const handleSubmit: SubmitHandler<ArticleForm> = async (values, event) => {
-    console.log("hello");
     articleMut.mutateAsync(values);
   };
   return (
-    <div class="flex w-11/12 max-w-prose flex-col items-center justify-start gap-8 rounded-3xl border-t-4 border-fuchsia-600 bg-white px-4 py-12 shadow-xl lg:p-16 ">
+    <div class="relative flex w-11/12 max-w-prose flex-col items-center justify-start gap-8 rounded-3xl border-t-4 border-fuchsia-600 bg-white px-4 py-12 shadow-xl ">
+      <CustomButton
+        onClick={() => {
+          props.setSubmitVis(false);
+        }}
+        class="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2 bg-red-600 p-2 text-center hover:bg-red-900 active:bg-red-900"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-8 w-8"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </CustomButton>
       <h2 class="text-2xl font-bold lg:text-3xl">Submit article/interview</h2>
       <Form onSubmit={handleSubmit}>
-        <h3>Link to article:</h3>
-        <Field name="link">
-          {(field, props) => (
-            <>
-              <input {...props} type="text" required />
-              {field.error && <div>{field.error}</div>}
-            </>
-          )}
-        </Field>
-        <h3>Short description of article:</h3>
-        <Field name="description">
-          {(field, props) => (
-            <>
-              <input {...props} type="text" required />
-              {field.error && <div>{field.error}</div>}
-            </>
-          )}
-        </Field>
-        <button type="submit">Submit</button>
+        <div class="m-4 flex flex-col items-center justify-center gap-4">
+          <h3 class="text-lg underline underline-offset-4">Link to article:</h3>
+          <Field name="link">
+            {(field, props) => (
+              <>
+                <textarea
+                  class="box-border w-11/12 resize-none rounded-lg border border-slate-600 p-4 focus-visible:outline-none"
+                  {...props}
+                  cols={60}
+                  rows={3}
+                  required
+                  autocomplete="off"
+                  placeholder="link"
+                />
+                {field.error && <div>{field.error}</div>}
+              </>
+            )}
+          </Field>
+          <h3 class="text-lg underline underline-offset-4">
+            Short description of article:
+          </h3>
+          <Field name="description">
+            {(field, props) => (
+              <>
+                <textarea
+                  class="box-border w-11/12 resize-none rounded-lg border border-slate-600 p-4 focus-visible:outline-none"
+                  {...props}
+                  cols={60}
+                  rows={9}
+                  required
+                  autocomplete="off"
+                  placeholder="description"
+                />
+                {field.error && <div>{field.error}</div>}
+              </>
+            )}
+          </Field>
+          <CustomButton type="submit">Submit</CustomButton>
+        </div>
       </Form>
     </div>
   );
@@ -62,6 +102,7 @@ const ArticleSubmit: Component = (props) => {
 const articles: Component = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [page, setPage] = createSignal(Number(searchParams.page ?? 1) - 1 ?? 0);
+  const [submitVis, setSubmitVis] = createSignal(false);
   const articlesData = articlesPagination(
     () => ({ page: page() }),
     () => ({
@@ -72,10 +113,31 @@ const articles: Component = () => {
   return (
     <div class="my-16 flex w-full flex-col items-center justify-start gap-8">
       <Suspense>
-        <ArticleSubmit />
+        <Show
+          when={submitVis()}
+          fallback={
+            <>
+              <div class="flex w-11/12 max-w-prose flex-col items-center justify-start gap-10 rounded-3xl border-t-4 border-fuchsia-600 bg-white py-12 shadow-xl ">
+                <h2 class="text-2xl font-bold lg:text-3xl">
+                  Submit new article/interview
+                </h2>
+                <CustomButton
+                  onClick={() => {
+                    setSubmitVis(true);
+                  }}
+                >
+                  Submit new!
+                </CustomButton>
+              </div>
+            </>
+          }
+        >
+          <ArticleSubmit setSubmitVis={setSubmitVis} />
+        </Show>
+        <h2 class="mt-8 text-4xl font-bold">Articles:</h2>
         <Show when={articlesData.data?.count}>
           {(data) => (
-            <div class="m-16 flex w-full max-w-md items-center justify-around">
+            <div class="flex w-full max-w-md items-center justify-around">
               <CustomButton
                 class={page() === 0 ? "invisible" : ""}
                 onClick={() => {
@@ -131,7 +193,7 @@ const articles: Component = () => {
       <Suspense>
         <Show when={articlesData.data?.count}>
           {(data) => (
-            <div class="m-16 flex w-full max-w-md items-center justify-around">
+            <div class="flex w-full max-w-md items-center justify-around">
               <CustomButton
                 class={page() === 0 ? "invisible" : ""}
                 onClick={() => {
