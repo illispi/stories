923d340d2cee4efc4d4697ef16393cd765a49428
illispi <44943503+illispi@users.noreply.github.com>
2023-09-21 11:12:55 +0300

    Suspense problems again, headers implemented

--- src/routes/user/data.tsx
+++ src/routes/user/data.tsx
@@ -1,4 +1,5 @@
 import { useQueryClient } from "@tanstack/solid-query";
+import type { ParentComponent } from "solid-js";
 import { ErrorBoundary, For, Show, Suspense, createSignal } from "solid-js";
 import { ErrorMessage, Navigate } from "solid-start";
 import { HttpStatusCode } from "solid-start/server";
@@ -11,6 +12,14 @@ import {
   removeTheir,
 } from "~/server/user/userMutations";
 import { getArticles, getPersonal, getTheirs } from "~/server/user/userQueries";
+
+const Box: ParentComponent = (props) => {
+  return (
+    <div class="my-8 flex w-11/12 max-w-2xl flex-col justify-between gap-16 rounded-3xl border-t-4 border-fuchsia-600 bg-white px-4 py-12 shadow-xl lg:p-16">
+      {props.children}
+    </div>
+  );
+};
 
 export const { routeData, Page } = ProtectedUser((session) => {
   //TODO test roles
@@ -40,8 +49,9 @@ export const { routeData, Page } = ProtectedUser((session) => {
       queryClient.invalidateQueries({ queryKey: ["getArticles"] }),
   }));
 
-  console.log(personal.data); //BUG without this, SSR doesnt work
-  console.log(their.data); //BUG without this, SSR doesnt work
+  // console.log(personal.data); //BUG without this, SSR doesnt work
+  // console.log(their.data); //BUG without this, SSR doesnt work
+  // console.log(articles.data); //BUG without this, SSR doesnt work
 
   return (
     <div class="flex flex-col items-center justify-start">
@@ -78,7 +88,7 @@ export const { routeData, Page } = ProtectedUser((session) => {
                 when={personal.data != "No personal poll data found"}
                 fallback={<p>No personal data found yet</p>}
               >
-                <>
+                <Box>
                   <CustomButton
                     onClick={() => {
                       removePersonalMut.mutateAsync();
@@ -86,17 +96,47 @@ export const { routeData, Page } = ProtectedUser((session) => {
                   >
                     Delete this personal poll data
                   </CustomButton>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Describe hospital:
+                  </h2>
                   <p>{personal.data?.describe_hospital}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    What kind of hospital care:
+                  </h2>
                   <p>{personal.data?.what_kind_of_care_after}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Your personality before:
+                  </h2>
                   <p>{personal.data?.personality_before}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Your personality after:
+                  </h2>
                   <p>{personal.data?.personality_after}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Other help beyond medication:
+                  </h2>
                   <p>{personal.data?.other_help}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Life goals after illness:
+                  </h2>
                   <p>{personal.data?.goals_after}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    How people responded to you:
+                  </h2>
                   <p>{personal.data?.responded_to_telling}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Your life satisfaction:
+                  </h2>
                   <p>{personal.data?.life_satisfaction_description}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    What others should know about illness:
+                  </h2>
                   <p>{personal.data?.what_others_should_know}</p>
+                  <h2 class="text-2xl font-bold lg:text-3xl">
+                    Have or not have schizophrenia:
+                  </h2>
                   <p>{personal.data?.not_have_schizophrenia_description}</p>
-                </>
+                </Box>
               </Show>
             </Suspense>
           </Show>
@@ -112,7 +152,7 @@ export const { routeData, Page } = ProtectedUser((session) => {
             <Suspense>
               <For each={their.data}>
                 {(their) => (
-                  <>
+                  <Box>
                     <CustomButton
                       onClick={() => {
                         removeTheirMut.mutateAsync({ id: their.id });
@@ -120,11 +160,19 @@ export const { routeData, Page } = ProtectedUser((session) => {
                     >
                       Delete this poll data
                     </CustomButton>
-
+                    <h2 class="text-2xl font-bold lg:text-3xl">
+                      Their personality before:
+                    </h2>
                     <p>{their.personality_before}</p>
+                    <h2 class="text-2xl font-bold lg:text-3xl">
+                      Their personality after:
+                    </h2>
                     <p>{their.personality_after}</p>
+                    <h2 class="text-2xl font-bold lg:text-3xl">
+                      What others should know about schizophrenia:
+                    </h2>
                     <p>{their.what_others_should_know}</p>
-                  </>
+                  </Box>
                 )}
               </For>
             </Suspense>
@@ -141,7 +189,7 @@ export const { routeData, Page } = ProtectedUser((session) => {
             <Suspense>
               <For each={articles.data}>
                 {(article) => (
-                  <>
+                  <Box>
                     <CustomButton
                       onClick={() => {
                         removeArticleMut.mutateAsync({ id: article.id });
@@ -152,7 +200,7 @@ export const { routeData, Page } = ProtectedUser((session) => {
 
                     <p>{article.link}</p>
                     <p>{article.description}</p>
-                  </>
+                  </Box>
                 )}
               </For>
             </Suspense>
