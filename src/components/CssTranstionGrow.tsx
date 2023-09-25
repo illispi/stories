b4f3e7fc576bdd7884bca8292a86aa11e509a2f8
illispi <44943503+illispi@users.noreply.github.com>
2023-09-25 11:45:57 +0300

    tweaks

--- src/components/CssTranstionGrow.tsx
+++ src/components/CssTranstionGrow.tsx
@@ -1,7 +1,7 @@
 import type { ParentComponent } from "solid-js";
 
 const CssTranstionGrow: ParentComponent<{
-  duration: number;
+  duration?: number;
   visible: boolean;
 }> = (props) => {
   return (
@@ -9,7 +9,9 @@ const CssTranstionGrow: ParentComponent<{
       style={{
         display: "grid",
         "grid-template-rows": `${props.visible ? "1fr" : "0fr"}`,
-        transition: `grid-template-rows ${props.duration}s ease-in-out`,
+        transition: `grid-template-rows ${
+          props.duration ?? "0.5"
+        }s ease-in-out`,
       }}
     >
       <div style={{ overflow: "hidden", "grid-row": "1 / span 2" }}>
