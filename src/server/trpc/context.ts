1605451a95c4799d2b3953786bef90aa9254232d
illispi <44943503+illispi@users.noreply.github.com>
2023-10-02 08:26:37 +0300

    context work

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,16 +1,27 @@
 import type { inferAsyncReturnType } from "@trpc/server";
 import type { createSolidAPIHandlerContext } from "solid-start-trpc";
+import { db } from "../server";
+import { Session } from "lucia";
+import { auth } from "~/auth/lucia";
 
-export const createContextInner = async (
-  opts: createSolidAPIHandlerContext
-) => {
+interface CreateInnerContextOptions
+  extends Partial<createSolidAPIHandlerContext> {
+  session: Session | null;
+}
+
+export const createContextInner = async (opts: CreateInnerContextOptions) => {
   return {
-    ...opts,
+    db,
+    session: opts.session,
   };
 };
 
-export const createContext = async (opts: createSolidAPIHandlerContext) => {
-  return await createContextInner(opts);
+export const createContext = async (opts: CreateInnerContextOptions) => {
+  const authRequest = auth.handleRequest(opts.req!);
+  const session = await authRequest.validate();
+
+  const contextInner = await createContextInner({ session });
+  return { ...contextInner, req: opts.req, res: opts.res };
 };
 
-export type IContext = inferAsyncReturnType<typeof createContext>;
+export type IContext = inferAsyncReturnType<typeof createContextInner>;
--- src/server/trpc/utils.ts
+++ src/server/trpc/utils.ts
@@ -1,7 +1,46 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import type { IContext } from "./context";
 
 export const t = initTRPC.context<IContext>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const apiProcedure = publicProcedure.use((opts) => {
+  if (!opts.ctx.req || !opts.ctx.res) {
+    throw new Error("You are missing `req` or `res` in your call.");
+  }
+  return opts.next({
+    ctx: {
+      // We overwrite the context with the truthy `req` & `res`, which will also overwrite the types used in your procedure.
+      req: opts.ctx.req,
+      res: opts.ctx.res,
+    },
+  });
+});
+export const userProcedure = apiProcedure.use(async (opts) => {
+  if (opts.ctx.session) {
+    const user = await opts.ctx.db
+      .selectFrom("auth_user")
+      .select(["id", "role"])
+      .where("id", "=", opts.ctx.session.user?.userId)
+      .executeTakeFirstOrThrow();
+
+    if (user) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "User was not found",
+      });
+    }
+
+    return opts.next({
+      ctx: {
+        user,
+      },
+    });
+  }
+
+  throw new TRPCError({
+    code: "UNAUTHORIZED",
+    message: "You are not authorized",
+  });
+});
