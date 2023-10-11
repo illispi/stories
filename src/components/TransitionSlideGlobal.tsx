78215c8a296be22a0df11515d65e8ac4d43e5f21
illispi <44943503+illispi@users.noreply.github.com>
2023-10-11 06:51:42 +0300

    transGlobal doesnt work with just css transtion

--- src/components/TransitionSlideGlobal.tsx
+++ src/components/TransitionSlideGlobal.tsx
@@ -28,7 +28,6 @@ const TransitionSlideGlobal: ParentComponent = (props) => {
   return (
     <>
       <Transition
-        name="slide-fade"
         onBeforeExit={() => {
           setScrollPrev(scrollTo());
           setScrollTo(window.scrollY);
@@ -45,13 +44,77 @@ const TransitionSlideGlobal: ParentComponent = (props) => {
             window.scrollTo(0, Number(localStorage.getItem("scrollPos") ?? 0));
           }
 
+          const a = el.animate(
+            [
+              {
+                opacity: 0,
+                transform: "translate(100px)",
+                easing: "ease-out",
+              },
+              { opacity: 1, transform: "translate(0)" },
+            ],
+            {
+              duration: 300,
+            }
+          );
           setVis(true);
+          a.finished.then(done);
+        }}
+        onExit={(el, done) => {
+          const a = el.animate(
+            [
+              {
+                opacity: 1,
+                transform: "translate(0)",
+                easing: "ease-in",
+              },
+              { opacity: 0, transform: "translate(-100px)" },
+            ],
+            {
+              duration: 300,
+            }
+          );
+          a.finished.then(done);
         }}
         mode="outin"
       >
         {props.children}
       </Transition>
-      <Transition mode="outin" name="slide-fade">
+      <Transition
+        onEnter={(el, done) => {
+          const a = el.animate(
+            [
+              {
+                opacity: 0,
+                transform: "translate(100px)",
+                easing: "ease-out",
+              },
+              { opacity: 1, transform: "translate(0)" },
+            ],
+            {
+              duration: 300,
+            }
+          );
+          a.finished.then(done);
+        }}
+        onExit={(el, done) => {
+          const a = el.animate(
+            [
+              {
+                opacity: 1,
+                transform: "translate(0)",
+                easing: "ease-in",
+              },
+              { opacity: 0, transform: "translate(-100px)" },
+            ],
+            {
+              duration: 300,
+            }
+          );
+          a.finished.then(done);
+        }}
+        mode="outin"
+      >
         <Show when={vis()}>
           <Footer />
         </Show>
