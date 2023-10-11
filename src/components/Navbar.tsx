b19621637ee0cbc931b82e2a29f4a8ead4aacd11
illispi <44943503+illispi@users.noreply.github.com>
2023-10-11 06:45:48 +0300

    small fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,7 +101,7 @@ const Hamburger: Component<{
         </button>
       </div>
       <div
-        class="fixed right-0 top-0 z-30 h-screen w-screen bg-slate-950 transition-all duration-300"
+        class="fixed right-0 top-14 z-30 h-screen w-screen bg-slate-950 transition-all duration-300"
         classList={{
           ["opacity-0 invisible"]: !searchParams.nav,
           ["visible, opacity-40"]: searchParams.nav === "true",
