c44aba639bca95d95f8245f2e70710d8eac262ac
illispi <44943503+illispi@users.noreply.github.com>
2023-10-11 06:30:01 +0300

    transition css fade

--- src/components/TransitionFade.tsx
+++ src/components/TransitionFade.tsx
@@ -3,42 +3,7 @@ import { Transition } from "solid-transition-group";
 
 const TransitionFade: ParentComponent = (props) => {
   return (
-    <Transition
-      onEnter={(el, done) => {
-        const a = el.animate(
-          [
-            {
-              opacity: 0,
-              easing: "ease-out",
-            },
-            { opacity: 1 },
-          ],
-          {
-            duration: 300,
-            delay: 300,
-          }
-        );
-        a.finished.then(done);
-      }}
-      onExit={(el, done) => {
-        const a = el.animate(
-          [
-            {
-              opacity: 1,
-
-              easing: "ease-in",
-            },
-            { opacity: 0 },
-          ],
-          {
-            duration: 300,
-            delay: 300,
-          }
-        );
-        a.finished.then(done);
-      }}
-      mode="outin"
-    >
+    <Transition name="fade" mode="outin">
       {props.children}
     </Transition>
   );
