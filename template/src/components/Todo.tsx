4406e77a6f551eba8e6ad6603ce68e3beeea7d8f
illispi <44943503+illispi@users.noreply.github.com>
2024-01-29 13:23:01 +0200

    bun test

---
+++ template/src/components/Todo.tsx
@@ -0,0 +1,27 @@
+import { createMutation } from '@tanstack/solid-query';
+import { app } from '~/app';
+import { handleEden } from '~/utils';
+
+interface TodoProps {
+  id: string;
+  data: string;
+}
+
+export default function Todo(props: TodoProps) {
+  const todoDelete = createMutation(() => ({
+    mutationFn: async () => handleEden(await app.api.todo[props.id]!.delete()),
+  }));
+  return (
+    <div class={'flex flex-row justify-center gap-4'}>
+      <pre>{props.data}</pre>
+      <button
+        class={
+          'rounded border-2 border-black bg-red-300 px-4 transition-all hover:bg-red-400 active:bg-red-400 disabled:cursor-not-allowed disabled:bg-red-400'
+        }
+        disabled={todoDelete.isPending}
+        onClick={() => todoDelete.mutate()}>
+        X
+      </button>
+    </div>
+  );
+}
