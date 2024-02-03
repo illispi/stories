4bed1776b2f509f91ace11a0dda1de048ff7ae15
illispi <44943503+illispi@users.noreply.github.com>
2024-02-03 07:33:44 +0200

    whole issue was missing show

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,17 +1,28 @@
 import { createQuery } from "@tanstack/solid-query";
-import { Component, Suspense } from "solid-js";
+import { Component, Show, Suspense } from "solid-js";
 import { eden } from "~/app";
 import { handleEden } from "~/utils";
 
 const test: Component = (props) => {
   const authQuery = createQuery(() => ({
     queryKey: ["test"],
-    queryFn: async () => handleEden(await eden.api.test.get()),
+    queryFn: async () => {
+      await new Promise((resolve, _reject) => {
+        setTimeout(resolve, 1000);
+      });
+      return "test";
+    },
+    // queryFn: async () => {
+    //   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //   return data.json();
+    // },
   }));
 
   return (
     <div>
-      <Suspense>{authQuery.data}</Suspense>
+      <Suspense>
+        <Show when={authQuery.data}>{authQuery.data}</Show>
+      </Suspense>
     </div>
   );
   // return <div>Hello</div>;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,14 @@
-import type { EdenFetchError } from '@elysiajs/eden/src/errors';
-import type { TSchema, Static } from '@sinclair/typebox/type';
-import { Check } from '@sinclair/typebox/value';
+import type { EdenFetchError } from "@elysiajs/eden/src/errors";
+import type { TSchema, Static } from "@sinclair/typebox/type";
+import { Check } from "@sinclair/typebox/value";
 import {
   Errors,
   SetErrorFunction,
   DefaultErrorFunction,
-} from '@sinclair/typebox/errors';
+} from "@sinclair/typebox/errors";
 
 SetErrorFunction((error) => {
-  if (typeof error.schema.error === 'string') return error.schema.error;
+  if (typeof error.schema.error === "string") return error.schema.error;
   return DefaultErrorFunction(error);
 });
 
@@ -45,5 +45,6 @@ export function handleEden<T>(
   }
 ): T {
   if (response.error) throw response.error;
+  console.log(response.data, "response");
   return response.data;
 }
