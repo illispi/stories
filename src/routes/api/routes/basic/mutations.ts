71ea7ed489c20afe36963c6c50848ed8fac78abe
illispi <44943503+illispi@users.noreply.github.com>
2024-02-12 10:25:58 +0200

    schema problem?

--- src/routes/api/routes/basic/mutations.ts
+++ src/routes/api/routes/basic/mutations.ts
@@ -1,56 +1,30 @@
-import {
-  personalQuestionsSchema,
-  theirQuestionsSchema,
-} from "~/types/zodFromTypes";
 import { z } from "zod";
 import { Elysia, t } from "elysia";
 import { db } from "../../db";
 import { derive } from "../derive";
+import {
+  personalQuestionsSchema,
+  theirQuestionsSchema,
+} from "~/types/zodFromTypes";
 
 export const basicMutationsRoute = new Elysia({ prefix: "/basic/post" })
   .use(derive)
-  .post(
-    "/personal",
-    async (context) => {
-      personalQuestionsSchema.parse(context.body);
+  .post("/personal", async (context) => {
+    personalQuestionsSchema.parse(context.body);
 
-      const existsAlready = await db
-        .selectFrom("Personal_questions")
-        .innerJoin("auth_user", "Personal_questions.user", "auth_user.id")
-        .select(["Personal_questions.id"])
-        .where("auth_user.id", "=", context.session?.userId)
-        .executeTakeFirst();
+    const existsAlready = await db
+      .selectFrom("Personal_questions")
+      .innerJoin("auth_user", "Personal_questions.user", "auth_user.id")
+      .select(["Personal_questions.id"])
+      .where("auth_user.id", "=", context.session?.userId)
+      .executeTakeFirst();
 
-      if (existsAlready) {
-        context.set.status = 500;
-        throw new Error("Your personal poll data already exists");
-      } else {
-        const insertion = await db
-          .insertInto("Personal_questions")
-          .values({
-            ...context.body,
-            user: context.session?.userId,
-            accepted: null,
-          })
-          .executeTakeFirst();
-
-        if (!insertion) {
-          context.set.status = 500;
-          throw new Error("Insertion to database failed");
-        }
-      }
-
-      return "Added succesfully";
-    },
-    { body: t.Object({}) }
-  )
-  .post(
-    "/their",
-    async (context) => {
-      theirQuestionsSchema.parse(context.body);
-
+    if (existsAlready) {
+      context.set.status = 500;
+      throw new Error("Your personal poll data already exists");
+    } else {
       const insertion = await db
-        .insertInto("Their_questions")
+        .insertInto("Personal_questions")
         .values({
           ...context.body,
           user: context.session?.userId,
@@ -62,45 +36,57 @@ export const basicMutationsRoute = new Elysia({ prefix: "/basic/post" })
         context.set.status = 500;
         throw new Error("Insertion to database failed");
       }
-      return "Added succesfully";
-    },
+    }
 
-    { body: t.Object({}) }
-  )
-  .post(
-    "/article",
-    async (context) => {
-      z.object({
-        link: z
-          .string()
-          .max(1000, { message: "Must be less than 1000 characters long" })
-          .min(5, { message: "Must be more than 5 characters long" })
-          .trim(),
-        description: z
-          .string()
-          .max(500, { message: "Must be less than 1000 characters long" })
-          .min(5, { message: "Must be more than 5 characters long" })
-          .trim(),
-      }).parse(context.body);
+    return "Added succesfully";
+  })
+  .post("/their", async (context) => {
+    theirQuestionsSchema.parse(context.body);
 
-      const insertion = await db
-        .insertInto("Articles")
-        .values({
-          ...context.body,
-          user: context.session?.userId,
-          accepted: null,
-        })
-        .executeTakeFirst();
+    const insertion = await db
+      .insertInto("Their_questions")
+      .values({
+        ...context.body,
+        user: context.session?.userId,
+        accepted: null,
+      })
+      .executeTakeFirst();
 
-      if (!insertion) {
-        context.set.status = 500;
-        throw new Error("Insertion to database failed");
-      }
-      return "Added succesfully";
-    },
+    if (!insertion) {
+      context.set.status = 500;
+      throw new Error("Insertion to database failed");
+    }
+    return "Added succesfully";
+  })
+  .post("/article", async (context) => {
+    z.object({
+      link: z
+        .string()
+        .max(1000, { message: "Must be less than 1000 characters long" })
+        .min(5, { message: "Must be more than 5 characters long" })
+        .trim(),
+      description: z
+        .string()
+        .max(500, { message: "Must be less than 1000 characters long" })
+        .min(5, { message: "Must be more than 5 characters long" })
+        .trim(),
+    }).parse(context.body);
 
-    { body: t.Object({}) }
-  )
+    const insertion = await db
+      .insertInto("Articles")
+      .values({
+        ...context.body,
+        user: context.session?.userId,
+        accepted: null,
+      })
+      .executeTakeFirst();
+
+    if (!insertion) {
+      context.set.status = 500;
+      throw new Error("Insertion to database failed");
+    }
+    return "Added succesfully";
+  })
   .onError(({ error }) => {
     return error.message;
   });
