d6afa26bb0f85f7611e6b6016d8ca775a8814784
illispi <44943503+illispi@users.noreply.github.com>
2024-05-05 18:20:12 +0300

    e

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,14 @@
 import { imagetools } from "vite-imagetools";
-import solid from "solid-start/vite";
+import { defineConfig } from "@solidjs/start/config";
 import dotenv from "dotenv";
-import { PluginOption, defineConfig } from "vite";
 import devtools from "solid-devtools/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-export default defineConfig(() => {
-  dotenv.config();
+export default defineConfig( {
   //[ssr: process.env.NODE_ENV === "DEV" ? false : true }
-  return {
-    plugins: [
+  
+    // plugins: [
       // visualizer({
       //   template: "treemap", // or sunburst
       //   open: true,
@@ -25,18 +23,18 @@ export default defineConfig(() => {
       //   /* features options - all disabled by default */
       //   autoname: true, // e.g. enable autoname
       // }),
-      imagetools(),
-      solid({ ssr: true }),
-    ],
-    server: {
-      strictPort: true,
-      port: 3000,
-      host: true,
-      https: process.env.NODE_ENV === "production" ? true : false,
-    },
+    //   imagetools(),
+    //   solid({ ssr: true }),
+    // ],
+    // server: {
+    //   strictPort: true,
+    //   port: 3000,
+    //   host: true,
+    //   https: process.env.NODE_ENV === "production" ? true : false,
+    // },
     /*  build: { target: "es2020" },
     optimizeDeps: { esbuildOptions: "es2020" }, */
-  };
+  
 });
 
 //NOTE solidjs dev tools uncomment import in entry-client.tsx aswell
