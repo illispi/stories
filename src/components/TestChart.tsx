4b4a60f1c0263a442ab12e32fcd5d1959d6163a4
illispi <44943503+illispi@users.noreply.github.com>
2024-05-07 16:03:39 +0300

    tests remove these!!!!!

---
+++ src/components/TestChart.tsx
@@ -0,0 +1,62 @@
+import { mergeRefs } from "@solid-primitives/refs";
+import type { AxisOptions, BarChartOptions } from "chartist";
+import { BarChart } from "chartist";
+import type { Component } from "solid-js";
+import {
+	Show,
+	Suspense,
+	createEffect,
+	createUniqueId,
+	onCleanup,
+} from "solid-js";
+import type { ChartistData } from "~/types/types";
+import "../styles/index.css";
+import { useIsRouting } from "@solidjs/router";
+
+interface Adds extends BarChartOptions<AxisOptions, AxisOptions> {
+	height?: string;
+}
+
+const BarChartCustomtest = (props) => {
+	let bar: BarChart;
+	const id = 1;
+
+	// createEffect(() => {
+	// 	setTimeout(() => {
+	// 		console.log("after");
+	// 		bar = new BarChart(
+	// 			`#chartBar1`,
+	// 			{
+	// 				series: [4, 7],
+	// 				labels: ["hello", "test"],
+	// 			},
+	// 			{
+	// 				...props.options,
+	// 				width: "100%",
+	// 				chartPadding: 25,
+	// 				axisX: {
+	// 					onlyInteger: true,
+	// 				},
+	// 				axisY: { offset: 80, scaleMinSpace: 25 },
+	// 				distributeSeries: true,
+	// 				horizontalBars: true,
+	// 			}, //BUG use mergeProps if you want defaults
+	// 		);
+	// 	}, 500);
+	// });
+
+	return (
+		<Suspense>
+			<div>
+				{console.log("before")}
+				<div class="flex w-full flex-col items-center justify-center h-64">
+					<div class={`h-64 w-full lg:w-11/12`} id={`chartBar1`} />
+				</div>
+			</div>
+		</Suspense>
+	);
+};
+
+export default BarChartCustomtest;
+
+//NOTE index.css is global but you can get over it with module.css, see start.solidjs.com "styling"
---
+++ src/routes/test.tsx
@@ -0,0 +1,20 @@
+import type { ParentComponent } from "solid-js";
+import {
+	Suspense
+} from "solid-js";
+import BarChartCustomtest from "~/components/TestChart";
+
+const Test: ParentComponent = () => {
+	return (
+		<Suspense>
+			<div>
+				<BarChartCustomtest />
+			</div>
+		</Suspense>
+	);
+};
+
+export default Test;
+
+//TODO might want to change ask first if you have told anybody and then who
+//TODO what_others_Should_know and not_have_schizophrenia_description need custom logic in text to show yes and no separately, backend solution might be good
